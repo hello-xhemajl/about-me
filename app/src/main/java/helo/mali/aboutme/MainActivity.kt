1d6dca6fff2c95864e4781e2dabcdcdaee556d59
xhemajl <xhemajl@post.com>
2020-04-25 10:03:19 +0200

    Add nick name adder

--- app/src/main/java/helo/mali/aboutme/MainActivity.kt
+++ app/src/main/java/helo/mali/aboutme/MainActivity.kt
@@ -1,12 +1,39 @@
 package helo.mali.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            bakeNickName(it)
+        }
+    }
+
+    private fun bakeNickName(button: View?) {
+        val nicknameTextView: TextView = findViewById(R.id.nick_name_text_view)
+        val nicknameEditText: EditText = findViewById(R.id.nick_name_edit_text)
+
+        nicknameTextView.text = nicknameEditText.text
+        nicknameTextView.visibility = View.VISIBLE
+
+        nicknameEditText.visibility = View.GONE
+        button!!.visibility =View.GONE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(button.windowToken, 0)
+
     }
 }
